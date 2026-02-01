b92ee4f3510eaea6ba3849c593226ab5353ed844
pick-cee <akinloluwaolumuyide@gmail.com>
2026-02-01 22:57:58 +0100

    Add common response and pagination utilies

---
+++ internal/utils/pagination.go
@@ -0,0 +1,65 @@
+package utils
+
+import (
+	"net/http"
+	"strconv"
+
+	"gorm.io/gorm"
+)
+
+// Generic paginated response
+type PaginatedResponse[T any] struct {
+	Page       int   `json:"page"`
+	Limit      int   `json:"limit"`
+	TotalItems int64 `json:"total_items"`
+	TotalPages int   `json:"total_pages"`
+	Data       []T   `json:"data"`
+}
+
+// Pagination params
+type PaginationParams struct {
+  Page  int
+  Limit int
+}
+
+func GetPaginationParams(r *http.Request) PaginationParams {
+	page, _ := strconv.Atoi(r.URL.Query().Get("page"))
+	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+
+	if page <= 0 {
+		page = 1
+	}
+	if limit <= 0 {
+		limit = 10
+	}
+	if limit > 100 {
+		limit = 100
+	}
+
+	return PaginationParams{
+		Page:  page,
+		Limit: limit,
+	}
+}
+
+func Paginate(params PaginationParams) func (db *gorm.DB) *gorm.DB {
+	return func (db *gorm.DB) *gorm.DB {
+		offset := (params.Page - 1) * params.Limit
+		return db.Offset(offset).Limit(params.Limit)
+	}
+}
+
+func NewPaginationResponse[T any](data []T, total int64, params PaginationParams) PaginatedResponse[T] {
+	totalPages := int(total) / params.Limit
+	if int(total) % params.Limit != 0 {
+		totalPages++
+	}
+
+	return PaginatedResponse[T]{
+		Page: params.Page,
+		Limit: params.Limit,
+		TotalItems: total,
+		TotalPages: totalPages,
+		Data: data,
+	}
+}
---
+++ internal/utils/response.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SuccessResponse(c *gin.Context, statusCode int, data interface{}) {
+	c.JSON(statusCode, gin.H{
+		"success": true,
+		"data": data,
+	})
+}
+
+func ErrorResponse(c *gin.Context, statusCode int, message string) {
+	c.JSON(statusCode, gin.H{
+		"success": false,
+		"message": message,
+	})
+}
+
+func ValidationErrorResponse(c *gin.Context, error string) {
+	c.JSON(http.StatusBadRequest, gin.H{
+		"success": false,
+		"errors": error,
+	})
+}
